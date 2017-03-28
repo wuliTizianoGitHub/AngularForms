93db8f142d0686fdb95023c09ea7eccea1e2bc96
wuliTizianoGitHub <TizianoMail@163.com>
2017-03-28 18:46:52 +0800

    提交

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<hero-form></hero-form>',
 })
 export class AppComponent {
-  title = 'app works!';
 }
---
+++ src/app/hero-form.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { Hero } from './models/hero';
+
+@Component({
+    moduleId: module.id,  //这个设置了基础地址，用于从相对模块路径中加载模板。
+    selector: 'hero-form',
+    templateUrl: './hero-form.component.html',
+    // styleUrls:['./forms.css']
+})
+
+export class HeroFormComponent {
+    powers = ["智障","无聊"];
+    model = new Hero(18, '233', this.powers[0], "就很难受（╯' - ')╯︵ ┻━┻");
+
+    submitted = false;
+
+    onSubmit() { this.submitted = true; }
+
+    //TODO:当我们的方法完成时移除它。
+    get diagnostic() { return JSON.stringify(this.model); }
+}
